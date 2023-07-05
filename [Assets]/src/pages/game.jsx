3cf2fdad00beaa35d4603a87e6cee5da9a786a43
Zachary Chan <zca121@sfu.ca>
2023-07-05 02:30:08 -0700

    Add prop checking

--- [Assets]/src/pages/game.jsx
+++ [Assets]/src/pages/game.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 import { Grid, Button } from '@mui/material';
 
@@ -12,6 +13,68 @@ import { useLocalStorage } from '../utils/hooks/use_local_storage';
 import { placeEnemyShips } from '../utils/ship_placement';
 import { makeRandomShot } from '../utils/ai_logic/random';
 import { INITIAL_GAME_STATE } from '../utils/constants';
+
+const GameContent = (props) => {
+  const playerBattleGrid = props.playerBattleGrid;
+  const setPlayerBattleGrid = props.setPlayerBattleGrid;
+  const enemyShipGrid = props.enemyShipGrid;
+  const setGameLog = props.setGameLog;
+  const gameLog = props.gameLog;
+  const gameState = props.gameState;
+  const setGameState = props.setGameState;
+  const playerShipGrid = props.playerShipGrid;
+  const enemyBattleGrid = props.enemyBattleGrid;
+  const handleForfeit = props.handleForfeit;
+
+  return (
+    <Grid container direction="row" justifyContent="center" alignItems="center">
+      <Grid item xs={6}>
+        <ShipGrid
+          playerShipGrid={playerShipGrid}
+          enemyBattleGrid={enemyBattleGrid}
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <BattleGrid
+          gameState={gameState}
+          setGameState={setGameState}
+          playerBattleGrid={playerBattleGrid}
+          setPlayerBattleGrid={setPlayerBattleGrid}
+          enemyShipGrid={enemyShipGrid}
+          setGameLog={setGameLog}
+          gameLog={gameLog}
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <GameLogDisplay
+          gameLog={gameLog}
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <Button
+          color='error'
+          variant='contained'
+          onClick={handleForfeit}
+        >
+          Forfeit
+        </Button>
+      </Grid>
+    </Grid>
+  );
+}
+
+GameContent.propTypes = {
+  playerShipGrid: propTypes.array.isRequired,
+  enemyBattleGrid: propTypes.array.isRequired,
+  gameState: propTypes.object.isRequired,
+  setGameState: propTypes.func.isRequired,
+  playerBattleGrid: propTypes.array.isRequired,
+  setPlayerBattleGrid: propTypes.func.isRequired,
+  enemyShipGrid: propTypes.array.isRequired,
+  setGameLog: propTypes.func.isRequired,
+  gameLog: propTypes.array.isRequired,
+  handleForfeit: propTypes.func.isRequired,
+};
 
 const Game = (props) => {
   const setStats = props.setStats;
@@ -41,46 +104,25 @@ const Game = (props) => {
   // place enemy ships randomly
   React.useEffect(placeEnemyShipsIfNeeded, [gameState]);
 
-  // Note: add an reset game option
-  console.log(gameState)
-
   if (gameState.allPlayerShipsPlaced && gameState.playerReadyToPlay) {
     if (gameState.playerTurn) {
       return (
         <>
-          <Grid container direction="row" justifyContent="center" alignItems="center">
-            <Grid item xs={6}>
-              <ShipGrid
-                playerShipGrid={playerShipGrid}
-                enemyBattleGrid={enemyBattleGrid}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <BattleGrid
-                gameState={gameState}
-                setGameState={setGameState}
-                playerBattleGrid={playerBattleGrid}
-                setPlayerBattleGrid={setPlayerBattleGrid}
-                enemyShipGrid={enemyShipGrid}
-                setGameLog={setGameLog}
-                gameLog={gameLog}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <GameLogDisplay
-                gameLog={gameLog}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <Button
-                color='error'
-                variant='contained'
-                onClick={handleForfeit}
-              >
-                Forfeit
-              </Button>
-            </Grid>
-          </Grid>
+          <GameContent
+            gameState={gameState}
+            setGameState={setGameState}
+            playerBattleGrid={playerBattleGrid}
+            setPlayerBattleGrid={setPlayerBattleGrid}
+            enemyBattleGrid={enemyBattleGrid}
+            setEnemyBattleGrid={setEnemyBattleGrid}
+            playerShipGrid={playerShipGrid}
+            setPlayerShipGrid={setPlayerShipGrid}
+            enemyShipGrid={enemyShipGrid}
+            setEnemyShipGrid={setEnemyShipGrid}
+            gameLog={gameLog}
+            setGameLog={setGameLog}
+            handleForfeit={handleForfeit}
+          />
         </>
       );
     } else {
@@ -101,6 +143,10 @@ const Game = (props) => {
       />
     );
   }
+};
+
+Game.propTypes = {
+  setStats: propTypes.func.isRequired,
 };
 
 export default Game;
