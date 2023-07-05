a6e25854c1a50285723b29f86cca87b72016d434
Zachary Chan <zca121@sfu.ca>
2023-07-05 03:09:28 -0700

    fix missing unique key

--- [Assets]/src/components/game/selection_grid.jsx
+++ [Assets]/src/components/game/selection_grid.jsx
@@ -5,14 +5,6 @@ import { Button, Typography, Grid } from "@mui/material";
 import GridTile from "./grid_tile.jsx";
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 
-const PlaceholderMarker = ({ squareSize }) => (
-  <Grid item>
-    <Typography sx={{fontSize: `${squareSize}rem`,textAlign: "center",width: `${squareSize}rem`,marginRight: '1rem'}}>
-      {" "}
-    </Typography>
-  </Grid>
-)
-
 const ColumnMarker = ({ index, squareSize }) => (
   <Grid item key={`col${index}`}>
     <Typography
@@ -35,10 +27,17 @@ const RowMarker = ({ index, squareSize }) => (
 
 const SelectionGrid = (props) => {
   const { grid, legend, selectedSquare, setSelectedSquare, squareSize, squareSpacing, disableGridMarkers, disableClick, onClick } = props;
-  
-  const placeholderMarker = <PlaceholderMarker squareSize={squareSize} />
-  const columnMarkers = !disableGridMarkers ? [placeholderMarker, ...grid[0].map((_, i) => <ColumnMarker index={i} squareSize={squareSize} />)] : [];
-  const rowMarkers = !disableGridMarkers ? grid.map((_, i) => <RowMarker index={i} squareSize={squareSize} />) : [];
+
+  const columnMarkers = !disableGridMarkers ? [
+    <Grid item key="placeholderMarker">
+      <Typography sx={{fontSize: `${squareSize}rem`,textAlign: "center",width: `${squareSize}rem`,marginRight: '1rem'}}>
+        {" "}
+      </Typography>
+    </Grid>,
+    ...grid[0].map((_, i) => <ColumnMarker index={i} squareSize={squareSize} key={i} />)
+  ] : [];
+
+  const rowMarkers = !disableGridMarkers ? grid.map((_, i) => <RowMarker index={i} squareSize={squareSize} key={i} />) : [];
 
   return (
     <>
