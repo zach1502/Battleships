c810d8decdac24996fcf90f0f1ad43034999e41f
Zachary Chan <zca121@sfu.ca>
2023-07-08 11:25:59 -0700

    Reformat with Grid

--- [Assets]/src/pages/settings.jsx
+++ [Assets]/src/pages/settings.jsx
@@ -1,90 +1,96 @@
-        import React from "react";
-        import { Grid, Button, Typography, Slider, Box} from "@mui/material";
+import React from "react";
+import { Grid, Button, Typography, Slider } from "@mui/material";
 
-        function valuetext(value) {
-            return `${value}°C`;
-        }
+function valuetext(value) {
+  return `${value}°C`;
+}
 
-        const Settings = (props) => {
-            const [checked, setChecked] = React.useState(false); 
-            function handleChange(e) {
-                setChecked(e.target.checked);
-            }
-            return (
-                <>
-                <Grid container direction="column" justifyContent="space-evenly" alignItems="center">
-                    <Typography variant="h2" component="div" gutterBottom>
-                        Settings
-                    </Typography>
+const Settings = (props) => {
 
-                    <Typography variant="h5" component="div" align='center'>
-                        Master Volume
-                    </Typography>
+  const [checked, setChecked] = React.useState(false);
 
-                    <Box sx={{ width: 300}}>
-                        <Slider
-                            aria-label="Master Volume"
-                            defaultValue={30}
-                            getAriaValueText={valuetext}
-                            step={1}
-                            marks
-                            min={0}
-                            max={100}
-                            valueLabelDisplay="auto"
-                        />
-                    </Box>
-                        
-                    <Typography variant="h5" component="div" align='center'>
-                        Music
-                    </Typography>
+  const settings = props.settings;
+  const setSettings = props.setSettings;
 
-                    <Box sx={{ width: 300}}>
-                    <Slider
-                        aria-label="Music"
-                        defaultValue={30}
-                        getAriaValueText={valuetext}
-                        step={1}
-                        marks
-                        min={0}
-                        max={100}
-                        valueLabelDisplay="auto"
-                    />
-                    </Box>
+  function handleChange(e) {
+    setChecked(e.target.checked);
+  }
+  return (
+    <>
+      <Grid container direction="row" justifyContent="flex-start" alignItems="center">
+        <Grid item xs={12} align='center'>
+          <Typography variant="h2" component="div" gutterBottom>
+            Settings
+          </Typography>
+        </Grid>
 
-                    <Typography variant="h5" component="div" align='center'>
-                        Effects
-                    </Typography>
+        <Grid item xs={12} align='center'>
+          <Typography variant="h5" component="div" align='center'>
+            Master Volume
+          </Typography>
+          <Slider sx={{width: 300}}
+            aria-label="Master Volume"
+            defaultValue={30}
+            getAriaValueText={valuetext}
+            step={1}
+            min={0}
+            max={100}
+            valueLabelDisplay="auto"
+          />
+        </Grid>
 
-                    <Box sx={{ width: 300}}>
-                    <Slider
-                        aria-label="Effects"
-                        defaultValue={30}
-                        getAriaValueText={valuetext}
-                        step={1}
-                        marks
-                        min={0}
-                        max={100}
-                        valueLabelDisplay="auto"
-                    />
-                    </Box>   
+        <Grid item xs={12} align='center'>
+          <Typography variant="h5" component="div" align='center'>
+            Music
+          </Typography>
 
-                    <Typography variant="h5" component="div" align='center'>
-                        Gameplay 
-                    </Typography> 
+          <Slider sx={{width: 300}}
+            aria-label="Music"
+            defaultValue={30}
+            getAriaValueText={valuetext}
+            step={1}
+            min={0}
+            max={100}
+            valueLabelDisplay="auto"
+          />
+        </Grid>
 
-                    <Typography variant="body1" component="div" align='center'>
-                        Grid Hit: 'red' Grid Miss: 'white' Grid Blank: 'blue'
-                    </Typography> 
+        <Grid item xs={12} align='center'>
+          <Typography variant="h5" component="div" align='center'>
+            Effects
+          </Typography>
+          <Slider sx={{width: 300}}
+            aria-label="Effects"
+            defaultValue={30}
+            getAriaValueText={valuetext}
+            step={1}
+            min={0}
+            max={100}
+            valueLabelDisplay="auto"
+          />
+        </Grid>
 
-                    <Typography variant="body1" component="div" align='center'>
-                        Autosave? <input value = "test" type = "checkbox" onChange = {handleChange} /> Enable Animation?   <input value = "test" type = "checkbox" onChange = {handleChange}/>
-                    </Typography>
+        <Grid item xs={12} align='center'>
+          <Typography variant="h5" component="div" align='center'>
+            Gameplay
+          </Typography>
 
-                    <Button variant="contained"  align='center' href="/">Back to main menu</Button>
+          <Typography variant="body1" component="div" align='center'>
+            Grid Hit: 'red' Grid Miss: 'white' Grid Blank: 'blue'
+          </Typography>
+        </Grid>
 
-                </Grid>
-                </>
-            );
-        };
+        <Grid item xs={12} align='center'>
+          <Typography variant="body1" component="div" align='center'>
+            Autosave? <input value="test" type="checkbox" onChange={handleChange} /> Enable Animation?   <input value="test" type="checkbox" onChange={handleChange} />
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align='center'>
+          <Button variant="contained" align='center' href="/">Back to main menu</Button>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
-        export default Settings;
+export default Settings;
