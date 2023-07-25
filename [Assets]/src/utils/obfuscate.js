bb7bb9240a67e310547486285dd95ba01b1f1f08
Zachary Chan <zca121@sfu.ca>
2023-07-25 02:40:18 -0700

    use Obfuscation to thwart cheating

---
+++ [Assets]/src/utils/obfuscate.js
@@ -0,0 +1,21 @@
+const obfuscate = (str) => {
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+      result += (str.charCodeAt(i) + (i<<2)) + "-";
+    }
+    return result.slice(0, -1);
+  };
+  
+  const deobfuscate = (str) => {
+    let result = '';
+    const codeArray = str.split('-');
+    for (let i = 0; i < codeArray.length; i++) {
+      result += String.fromCharCode(codeArray[i]) - (i<<2);
+    }
+    return result;
+  };
+
+    export {
+        obfuscate,
+        deobfuscate,
+    };
