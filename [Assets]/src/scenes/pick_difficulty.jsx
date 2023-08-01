5165fd985f7d550e1bc667b61f951dd3d5e18012
Zachary Chan <zca121@sfu.ca>
2023-08-01 12:29:14 -0700

    Refactor Difficulty select screen

--- [Assets]/src/scenes/pick_difficulty.jsx
+++ [Assets]/src/scenes/pick_difficulty.jsx
@@ -1,93 +1,100 @@
-import React from 'react';
+import React, { useState, useCallback, useContext, createContext } from 'react';
 import propTypes from 'prop-types';
-
-import { Grid, Button, Typography, SvgIcon} from '@mui/material';
-
+import { Grid, Button, Typography, Box } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 
-const PickDifficulty = (props) => {
-    const selectedDifficulty = props.selectedDifficulty;
-    const setSelectedDifficulty = props.setSelectedDifficulty;
+const DifficultyContext = createContext();
 
-    const [tempDifficulty, setTempDifficulty] = React.useState(selectedDifficulty);
+const DifficultyButton = ({option}) => {
+    const { tempDifficulty, setTempDifficulty } = useContext(DifficultyContext);
+    return (
+        <Grid item xs={12} container justifyContent='center'>
+            <Button
+                variant="contained"
+                color={(tempDifficulty === option.value)? 'success' : 'primary'}
+                onClick={() => setTempDifficulty(option.value)}
+                sx={{
+                    width: '140px',
+                }}
+            >
+                {option.name}
+            </Button>
+        </Grid>
+    );
+};
+
+const StartGameButton = ({ setSelectedDifficulty, tempDifficulty }) => (
+    <Button
+        variant="contained"
+        color="success"
+        startIcon={<PlayArrowIcon/>}
+        onClick={() => {
+            if (tempDifficulty) {
+                setSelectedDifficulty(tempDifficulty);
+            }
+        }}
+    >
+        Start Game
+    </Button>
+);
+
+const MainMenuButton = () => (
+    <Button
+        variant="contained"
+        color="primary"
+        href="/"
+        startIcon={<MenuIcon/>}
+    >
+        Main Menu
+    </Button>
+);
+
+const DifficultyOptions = ({ options }) => (
+    <Grid item xs={12} container spacing={2} alignItems="center">
+        {options.map((option) => <DifficultyButton key={option.value} option={option} />)}
+    </Grid>
+);
+
+const PickDifficulty = ({ selectedDifficulty, setSelectedDifficulty }) => {
+    const [tempDifficulty, setTempDifficulty] = useState(selectedDifficulty);
 
     const difficultyOptions = [
-        {
-            name: 'Easy',
-            value: 'easy',
-        },
-        {
-            name: 'Medium',
-            value: 'medium',
-        },
-        {
-            name: 'Hard',
-            value: 'hard',
-        },
-        {
-            name: 'Impossible',
-            value: 'impossible',
-        }
+        { name: 'Easy', value: 'easy' },
+        { name: 'Medium', value: 'medium' },
+        { name: 'Hard', value: 'hard' },
+        { name: 'Impossible', value: 'impossible' }
     ];
 
-    const renderDifficultyOptions = React.useCallback(() => {
-        return difficultyOptions.map((option) => {
-            return (
-                <Grid item xs={12/difficultyOptions.length} key={option.value} container justifyContent='center' alignItems="center">
-                    <Button
-                        variant="contained"
-                        color={(tempDifficulty === option.value)? 'success' : 'primary'}
-                        value={option.value}
-                        onClick={() => setTempDifficulty(option.value)}
-                    >
-                        {option.name}
-                    </Button>
-                </Grid>
-            );
-        });
-    }, [tempDifficulty]);
+    const selectedDifficultyName = useCallback(() =>
+        difficultyOptions.find((option) => option.value === tempDifficulty)?.name || 'None',
+        [tempDifficulty, difficultyOptions]
+    );
 
     return (
-        <Grid container spacing={2} justifyContent='center' alignItems="center" >
-            <Grid item xs={12} container justifyContent='center' alignItems='center'>
-                <Typography variant="h2">
-                    Pick a Difficulty Level
-                </Typography>
-            </Grid>
-            <Grid item xs={12} container justifyContent='center' alignItems="center">
-                <Typography variant="h3">
-                    Selected: {difficultyOptions.find((option) => option.value === tempDifficulty)?.name || 'None'}
-                </Typography>
-            </Grid>
+        <DifficultyContext.Provider value={{ tempDifficulty, setTempDifficulty }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} container justifyContent='center' alignItems='center'>
+                    <Box display='flex' flexDirection='column' alignItems='center'>
+                        <Typography variant="h2">
+                            Pick a Difficulty Level
+                        </Typography>
+                        <Typography variant="h3">
+                            Selected: {selectedDifficultyName()}
+                        </Typography>
+                    </Box>
+                </Grid>
 
-            {renderDifficultyOptions()}
+                <DifficultyOptions options={difficultyOptions} />
 
-            <Grid item xs={6} container justifyContent='center' alignItems="center">
-                <Button
-                    variant="contained"
-                    color="success"
-                    startIcon={<PlayArrowIcon/>}
-                    onClick={() => {
-                        if (tempDifficulty) {
-                            setSelectedDifficulty(tempDifficulty);
-                        }
-                    }}
-                >
-                    Start Game
-                </Button>
+                <Grid item xs={6} container justifyContent='center' alignItems="center">
+                    <StartGameButton setSelectedDifficulty={setSelectedDifficulty} tempDifficulty={tempDifficulty} />
+                </Grid>
+                <Grid item xs={6} container justifyContent='center' alignItems="center">
+                    <MainMenuButton />
+                </Grid>
             </Grid>
-            <Grid item xs={6} container justifyContent='center' alignItems="center">
-                <Button
-                    variant="contained"
-                    color="primary"
-                    href="/"
-                    startIcon={<MenuIcon/>}
-                >
-                    Main Menu
-                </Button>
-            </Grid>
-        </Grid>
+        </DifficultyContext.Provider>
     );
 };
 
