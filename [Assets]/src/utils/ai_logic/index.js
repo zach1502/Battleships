72d549ab93baab112733e051fa7f1f2865f5e6a7
kha112 <kha112@sfu.ca>
2023-08-14 01:55:08 -0700

    add makeStrategicShot

--- [Assets]/src/utils/ai_logic/index.js
+++ [Assets]/src/utils/ai_logic/index.js
@@ -1,11 +1,13 @@
 import makeRandomShot from './random';
 import makeHuntAndSeekShot from './seek_and_hunt';
+import makeStrategicShot from './strategic';
 import makeHeatMapShot from './heat_map';
 import makeBestShot from './cheating';
 
 export {
   makeRandomShot,
   makeHuntAndSeekShot,
+  makeStrategicShot,
   makeHeatMapShot,
   makeBestShot,
 }
